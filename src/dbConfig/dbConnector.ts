fd164df302c6314b965c0ed230b293907d70e192
Alejandro <alejandro.pla@rithmi.com>
2021-11-10 12:55:17 +0100

    Adding comment in config dbConnector

--- src/dbConfig/dbConnector.ts
+++ src/dbConfig/dbConnector.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg'
 
 export default new Pool({
   user: 'postgres',
-  host: 'postgres',
+  host: 'postgres', // localhost in dev, postgres in pro
   database: 'belike-db',
   password: 'newPassword',
   port: 5432
