721f1b732afa46118fd9bf3629448c2f9da9bb46
Alejandro <alejandro.pla@rithmi.com>
2021-11-10 13:35:43 +0100

    Adding jest config

---
+++ src/users/users.controlles.test.ts
@@ -0,0 +1,7 @@
+// import UsersController from './users.controller'
+
+describe('auth', () => {
+  it('should resolve with true and valid userId for hardcoded token', async () => {
+    expect(true).toBe(true)
+  })
+})
