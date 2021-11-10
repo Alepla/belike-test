0fabcc88a539b703a3c888f842e599857cf6a27d
Alejandro <alejandro.pla@rithmi.com>
2021-11-10 12:45:44 +0100

    Changing host

--- src/dbConfig/dbConnector.ts
+++ src/dbConfig/dbConnector.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg'
 
 export default new Pool({
   user: 'postgres',
-  host: 'localhost',
+  host: 'postgres',
   database: 'belike-db',
   password: 'newPassword',
   port: 5432
